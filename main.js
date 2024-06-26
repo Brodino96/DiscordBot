3d4e61ccb60d30a8acdb25428717548a861ec311
Vincent Acanfora <brodino96@gmail.com>
2024-06-26 09:44:28 +0200

    First lines of codes

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-require("dotenv").config()
-console.log(process.env.DISCORD_TOKEN)
+const { Client, Events, GatewayIntentBits } = require("discord.js")
+const { token } = require("./token.json")
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(token)
