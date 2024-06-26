dd4bd0d28a71168f7e18748d053c7ee5d9f03755
Vincent Acanfora <brodino96@gmail.com>
2024-06-26 10:37:42 +0200

    Now logs messages

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const { Client, Events, GatewayIntentBits } = require("discord.js")
 const { token } = require("./token.json")
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+]})
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, function(ready) {
+	console.log(`Ready! Logged in as ${ready.user.tag}`);
 })
 
 client.login(token)
+
+client.on("messageCreate", async function(msg) {
+    console.log(msg)
+})
